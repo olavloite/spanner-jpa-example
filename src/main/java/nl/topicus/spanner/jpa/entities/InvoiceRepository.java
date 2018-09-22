eaf14d69ac8ecfd1795cb48f81850de3d63292b1
Olav Loite <koloite@gmail.com>
2018-09-22 11:07:15 +0200

    added example for using FORCE_INDEX_HINT in query

--- src/main/java/nl/topicus/spanner/jpa/entities/InvoiceRepository.java
+++ src/main/java/nl/topicus/spanner/jpa/entities/InvoiceRepository.java
@@ -1,6 +1,15 @@
 package nl.topicus.spanner.jpa.entities;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface InvoiceRepository extends CrudRepository<Invoice, Long> {
+
+  @Query(nativeQuery = true,
+      value = "SELECT * FROM Invoice@{FORCE_INDEX=IDX_INVOICE_INVOICENUMBER_DESCRIPTION} i where invoiceNumber=:invoiceNumber and description=:description")
+  Page<Invoice> findInvoiceByNumberAndDescription(Pageable pageable,
+      @Param("invoiceNumber") String invoiceNumber, @Param("description") String description);
 }
